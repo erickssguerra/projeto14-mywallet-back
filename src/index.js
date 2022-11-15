2e78ff33dcb089f2f0dbb16a73fdf36935f7191c
erickssguerra <erickssguerra@gmail.com>
2022-11-15 20:44:14 -0300

    feat: route /sign-up implemented (email and name validation + password encrypted + saving in the collection colUsers)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express, { json } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb"
+import bcrypt from "bcrypt"
+import joi from "joi"
 
 // configs
 dotenv.config()
@@ -23,19 +25,46 @@ const colUsers = dbMyWallet.collection("users")
 const colSessions = dbMyWallet.collection("sessions")
 
 // validation schemas
-const userSchema = joi.object({
+const schemaUser = joi.object({
     name: joi.string().required().min(2),
-    email: joi.string().required().email(),
+    email: joi.string().required().email()
 })
 
 // route "/sign-up"
-server.post("/sign-in", async (req, res) => {
+server.post("/sign-up", async (req, res) => {
     const { name, email, password } = req.body
+    const cryptedPassword = bcrypt.hashSync(password, 10)
+    const validation = schemaUser.validate({ name, email }, { abortEarly: false })
+
+    if (validation.error) {
+        const errorMessage = validation.error.details.map((detail) => detail.message)
+        res.status(422).send(errorMessage)
+        return
+    }
     
+    try {
+        const userExists = await colUsers.findOne({ email })
+
+        if (!userExists) {
+            await colUsers.insertOne({ name, email, password: cryptedPassword })
+            res.status(201).send({ message: "Usuário cadastrado com sucesso!" })
+            return
+        }
+
+        else {
+            res.status(401).send({ message: "E-mail já cadastrado." })
+            return
+        }
+    }
+    
+    catch (err) {
+        console.log(err)
+    }
 })
 
 // connection
 server.listen(5000, () => {
     console.log("Connected in port 5000!")
 })
+
 
