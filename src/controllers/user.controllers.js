8c2f717aafe5f3fa8fb65dff0daf4e7816ced3db
erickssguerra <erickssguerra@gmail.com>
2022-11-19 00:20:23 -0300

    refact: clean code in user.controllers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,26 +3,22 @@ import { colUsers } from "../database/collections.js"
 
 export async function postSignUp(req, res) {
     const { name, email, password } = req.user
-
     const cryptedPassword = bcrypt.hashSync(password, 10)
-
     try {
         const userExists = await colUsers.findOne({ email })
-
         if (!userExists) {
             await colUsers.insertOne({ name, email, password: cryptedPassword })
             res.status(201).send({ message: "Usuário cadastrado com sucesso!" })
             return
         }
-
         else {
             res.status(401).send({ message: "E-mail já cadastrado." })
             return
         }
     }
-
     catch (err) {
         console.log(err)
+        res.sendStatus(500)
     }
 }
 
